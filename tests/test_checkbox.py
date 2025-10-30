3a43f167740cbcebfe2b6ade8f42c7d324482c06
okansavas <158855291+okansavas@users.noreply.github.com>
2025-10-30 13:05:11 +0100

    erste checkbox angekreuzt, andere nicht

--- tests/test_checkbox.py
+++ tests/test_checkbox.py
@@ -3,10 +3,17 @@ import time
 
 
 def test_checkbox(driver):
+
     driver.get("https://the-internet.herokuapp.com/checkboxes")
+
     checkboxes = driver.find_elements(By.XPATH, "//input[@type='checkbox']")
-    for box in checkboxes:
-        if not box.is_selected():
-            box.click()
-    time.sleep(1)
-    assert all(box.is_selected() for box in checkboxes)
+
+    if not checkboxes[0].is_selected():
+        checkboxes[0].click()
+
+    if checkboxes[1].is_selected():
+        checkboxes[1].click()
+
+    time.sleep(2)
+    assert checkboxes[0].is_selected()
+    assert not checkboxes[1].is_selected()
